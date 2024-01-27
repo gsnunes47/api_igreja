5f084a8858458d9b40575a65a9b5c984e36e40ee
Gustavo Nunes <gustavompn06@gmail.com>
2024-01-27 10:31:40 -0300

    Mudança da chave URI do banco de dados

--- api/__init__.py
+++ api/__init__.py
@@ -1,9 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import os
+
 
 app = Flask('__main__', template_folder=r'sistema_igreja\templates')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE-URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://membros_user:vwSkzYDECJ6JTpCo05tZJa7dDqUbCVi7@dpg-cmqg10mg1b2c73d5nv3g-a/membros'
 
 database = SQLAlchemy(app)
 
--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import requests
 
+membros = requests.post(url='https://api-igreja.onrender.com/membros', json='')
 membros = requests.get(url='https://api-igreja.onrender.com/membros')
 print(membros)
