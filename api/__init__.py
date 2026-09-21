002b2e3bfe7f1cbb4d5f42aa22321ee2dc59c29e
Gustavo Nunes <gustavompn06@gmail.com>
2026-09-21 12:26:31 -0300

    Remove credenciais antigas

--- api/__init__.py
+++ api/__init__.py
@@ -5,8 +5,8 @@ from flask_bcrypt import Bcrypt
 from datetime import timedelta
 
 app = Flask('__main__', template_folder=r'sistema_igreja\templates')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://user:R67SICNGLtrJVGwYbySdZb6hUSLffm34@dpg-co23d3f79t8c73cgi3vg-a.oregon-postgres.render.com/membros_l0j3'
-app.config['SECRET_KEY'] = 'chavesecreta'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://usuario:senha@host/banco'
+app.config['SECRET_KEY'] = 'chave-local'
 
 database = SQLAlchemy(app)
 login_manager = LoginManager(app)
